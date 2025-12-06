33d3239aa937b2c328b2f6f90e78e1b6bb9529fd
MochcinBN <bounouara.mohcin@gmail.com>
2025-12-06 20:53:02 +0100

    Add fillable fields and user relationshipt to post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'excerpt',
+        'content',
+        'featured_image',
+        'status',
+        'user_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
